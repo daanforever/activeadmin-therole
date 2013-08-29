c6001d28c2416f46e8a0b0b0407d710d560c5de4
daanforever <daan.forever@gmail.com>
2013-08-29 18:08:12 +0400

    admin/roles.rb template

--- lib/generators/activeadmin_the_role/install/install_generator.rb
+++ lib/generators/activeadmin_the_role/install/install_generator.rb
@@ -1,3 +1,13 @@
-class ActiveadminTheRole::InstallGenerator < Rails::Generators::NamedBase
+class ActiveadminTheRole::InstallGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
+
+  def install
+    generate "devise:install"
+    generate "active_admin:install"
+    generate "devise", "User"
+    generate "the_role", "install"
+    rake "the_role_engine:install:migrations"
+    rake "db:migrate"
+    copy_file "app/admin/roles.rb", "app/admin/#{file_name}.rb"
+  end
 end
