4a64f1e607b15485f7dda738d7a954867a8cccab
daanforever <daan.forever@gmail.com>
2013-09-03 14:18:09 +0400

    new migration

--- lib/generators/activeadmin_the_role/install/install_generator.rb
+++ lib/generators/activeadmin_the_role/install/install_generator.rb
@@ -1,13 +1,25 @@
-class ActiveadminTheRole::InstallGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
+module ActiveadminTheRole
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+      source_root File.expand_path('../templates', __FILE__)
 
-  def install
-    generate "devise:install"
-    generate "active_admin:install"
-    generate "devise", "User"
-    generate "the_role", "install"
-    rake "the_role_engine:install:migrations"
-    rake "db:migrate"
-    copy_file "app/admin/roles.rb", "app/admin/#{file_name}.rb"
+      def self.next_migration_number(dirname)
+        orm = Rails.configuration.generators.options[:rails][:orm]
+        require "rails/generators/#{orm}"
+        "#{orm.to_s.camelize}::Generators::Base".constantize.next_migration_number(dirname)
+      end
+
+      def install
+        generate "devise:install"
+        generate "active_admin:install"
+        generate "devise", "User"
+        generate "the_role", "install"
+        rake "the_role_engine:install:migrations"
+        migration_template 'db/migrate/add_role_id_to_user.rb', 'db/migrate/add_role_id_to_user.rb'
+        rake "db:migrate"
+        copy_file "app/admin/roles.rb", "app/admin/roles.rb"
+      end
+    end
   end
 end
