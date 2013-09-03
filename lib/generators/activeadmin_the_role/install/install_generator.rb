f036f2599424cc34cd5a9df6b219230c9fd354eb
daanforever <daan.forever@gmail.com>
2013-09-03 14:32:44 +0400

    Added comments and Admin role for TheRole

--- lib/generators/activeadmin_the_role/install/install_generator.rb
+++ lib/generators/activeadmin_the_role/install/install_generator.rb
@@ -10,15 +10,26 @@ module ActiveadminTheRole
         "#{orm.to_s.camelize}::Generators::Base".constantize.next_migration_number(dirname)
       end
 
+      # Execute all steps for Devise, ActiveAdmin and TheRole
       def install
+        # install devise
         generate "devise:install"
+        # install activeadmin
         generate "active_admin:install"
+        # create User model
         generate "devise", "User"
+        # install TheRole
         generate "the_role", "install"
+        # create migration for TheRole
         rake "the_role_engine:install:migrations"
+        # copy our migration
         migration_template 'db/migrate/add_role_id_to_user.rb', 'db/migrate/add_role_id_to_user.rb'
+        # prepare database
         rake "db:migrate"
+        # copy our section
         copy_file "app/admin/roles.rb", "app/admin/roles.rb"
+        # create Admin role for TheRole
+        generate "the_role", "admin"
       end
     end
   end
